cc2bd52dbd31666456ef58ba81f640bd68d375db
Hendrix1995 <hgud55@naver.com>
2022-01-13 21:16:18 +0900

    ADD: Home 글씨 나타나는 애니메이션 추가 ADD: Home 스크롤 Icon 추가 ADD: Skills 나타나는 애니메이션 추가

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 
 function Home() {
     return (
-        <section className="main-catchphrase">
-            <span className="strong-word">쉽고 재미있는 세상</span>을 만들기 위해
-            <br />
-            <span className="strong-word">포기하지 않는 열정</span>으로
-            <br />
-            <span className="strong-word">도전하고 성장</span>하는 프론트엔드 개발자
-            <br />
-            <span className="strong-word">임현성</span>입니다.
+        <section className="home-page">
+            <Fade cascade delay={100}>
+                <div className="main-catchphrase">
+                    <span className="strong-word">쉽고 재미있는 세상</span>을 만들기 위해
+                    <br />
+                    <span className="strong-word">포기하지 않는 열정</span>으로
+                    <br />
+                    <span className="strong-word">도전하고 성장</span>하는 프론트엔드 개발자
+                    <br />
+                    <span className="strong-word">임현성</span>입니다.
+                </div>
+            </Fade>
+            <div className="main-scroll-icon-container">
+                <div className="main-scroll-icon">
+                    <div className="main-scroll-icon-down" />
+                </div>
+                <span className="main-scroll-icon-text">Scroll</span>
+            </div>
         </section>
     );
 }
--- src/view/Skills.tsx
+++ src/view/Skills.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
+
 import javascript from "../img/stack-icon/javascript-icon.png";
 import css from "../img/stack-icon/css-icon.png";
 import html from "../img/stack-icon/html-icon.png";
@@ -17,65 +19,73 @@ function Skills() {
             <div className="page-container-skills">
                 <div className="skills-box">
                     <div className="skills-box-title">Language</div>
-                    <div className="skills-box-content-box">
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={javascript} alt="JavaScript" />
-                            <div className="skills-box-stack-info">JavaScript</div>
+                    <Fade>
+                        <div className="skills-box-content-box">
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={javascript} alt="JavaScript" />
+                                <div className="skills-box-stack-info">JavaScript</div>
+                            </div>
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={css} alt="CSS" />
+                                <div className="skills-box-stack-info">CSS</div>
+                            </div>
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={html} alt="HTML" />
+                                <div className="skills-box-stack-info">HTML</div>
+                            </div>
                         </div>
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={css} alt="CSS" />
-                            <div className="skills-box-stack-info">CSS</div>
-                        </div>
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={html} alt="HTML" />
-                            <div className="skills-box-stack-info">HTML</div>
-                        </div>
-                    </div>
+                    </Fade>
                 </div>
                 <div className="skills-box">
                     <div className="skills-box-title">Frameworks & Libraries</div>
-                    <div className="skills-box-content-box">
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={react} alt="React" />
-                            <div className="skills-box-stack-info">React</div>
+                    <Fade delay={70}>
+                        <div className="skills-box-content-box">
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={react} alt="React" />
+                                <div className="skills-box-stack-info">React</div>
+                            </div>
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={sass} alt="SASS / SCSS" />
+                                <div className="skills-box-stack-info">SASS / SCSS</div>
+                            </div>
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={nodejs} alt="Node.js" />
+                                <div className="skills-box-stack-info">Node.js</div>
+                            </div>
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={express} alt="Express" />
+                                <div className="skills-box-stack-info">Express</div>
+                            </div>
                         </div>
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={sass} alt="SASS / SCSS" />
-                            <div className="skills-box-stack-info">SASS / SCSS</div>
-                        </div>
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={nodejs} alt="Node.js" />
-                            <div className="skills-box-stack-info">Node.js</div>
-                        </div>
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={express} alt="Express" />
-                            <div className="skills-box-stack-info">Express</div>
-                        </div>
-                    </div>
+                    </Fade>
                 </div>
                 <div className="skills-box">
                     <div className="skills-box-title">DB</div>
-                    <div className="skills-box-content-box">
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={mysql} alt="MySQL" />
-                            <div className="skills-box-stack-info">MySQL</div>
-                            <span className="skills-box-stack-info-details">(관계형 스키마 디자인, SQL 주요 문법)</span>
+                    <Fade delay={120}>
+                        <div className="skills-box-content-box">
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={mysql} alt="MySQL" />
+                                <div className="skills-box-stack-info">MySQL</div>
+                                <span className="skills-box-stack-info-details">(관계형 스키마 디자인, SQL 주요 문법)</span>
+                            </div>
                         </div>
-                    </div>
+                    </Fade>
                 </div>
                 <div className="skills-box">
                     <div className="skills-box-title">Tools</div>
-                    <div className="skills-box-content-box">
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={git} alt="Git" />
-                            <div className="skills-box-stack-info">Git</div>
+                    <Fade delay={170}>
+                        <div className="skills-box-content-box">
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={git} alt="Git" />
+                                <div className="skills-box-stack-info">Git</div>
+                            </div>
+                            <div className="skills-box-content">
+                                <img className="skills-box-stack-icon" src={aws} alt="AWS" />
+                                <div className="skills-box-stack-info">AWS</div>
+                                <span className="skills-box-stack-info-details">(EC2, RDS, S3를 이용한 배포 경험, PipeLine을 이용한 간단한 배포 자동화 경험)</span>
+                            </div>
                         </div>
-                        <div className="skills-box-content">
-                            <img className="skills-box-stack-icon" src={aws} alt="AWS" />
-                            <div className="skills-box-stack-info">AWS</div>
-                            <span className="skills-box-stack-info-details">(EC2, RDS, S3를 이용한 배포 경험, PipeLine을 이용한 간단한 배포 자동화 경험)</span>
-                        </div>
-                    </div>
+                    </Fade>
                 </div>
             </div>
         </section>
