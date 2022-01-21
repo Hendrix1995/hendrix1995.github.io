b2c9627603647515adb08a1029c4bf1104bf98e4
Hendrix1995 <hgud55@naver.com>
2022-01-21 19:07:11 +0900

    ADD: Loading 기본 파일

--- src/view/Main.tsx
+++ src/view/Main.tsx
@@ -1,17 +1,32 @@
+import React, { useState, useEffect } from "react";
+
 import Home from "./Home";
 import About from "./About";
 import Skills from "./Skills";
 import Project from "./Project";
 import Contact from "./Contact";
+import Loading from "../components/Loading";
 
 function Main() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setIsLoading(false), 1000);
+    }, []);
+
     return (
         <main>
-            <Home />
-            <About />
-            <Skills />
-            <Project />
-            <Contact />
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <>
+                    <Home />
+                    <About />
+                    <Skills />
+                    <Project />
+                    <Contact />
+                </>
+            )}
         </main>
     );
 }
